2decf05221f2339d44c37f7e0270bf216eee43f6
FirstName LastName <youremail@provider.com>
2022-02-17 01:09:38 -0500

    integrated api on shows page. completed functionality of delete button and like button on bio page. added fuction to sort the comments by dates.

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,78 +1,89 @@
-const showsArrey =[
-    {
-        date:1630900800000,
-        venue: "Ronald Lane ",
-        location: "San Francisco, CA",
-    },
-    {
-        date:1632196800000,
-        venue: "Pier 3 East ",
-        location: "San Francisco, CA",
-    },    
-    {
-        date:1634270400000,
-        venue: "View Lounge",
-        location: "San Francisco, CA",
-    },    
-    {
-        date:1636171200000,
-        venue: "Hyatt Agency",
-        location: "San Francisco, CA",
-    },    
-    {
-        date:1637902800000,
-        venue: "Moscow Center",
-        location: "San Francisco, CA",
-    },
-    {
-        date:1639544400000,
-        venue: "Press Club",
-        location: "San Francisco, CA",
-    },
-];
+// const showsArrey = [
+//   {
+//     date: 1630900800000,
+//     venue: "Ronald Lane ",
+//     location: "San Francisco, CA",
+//   },
+//   {
+//     date: 1632196800000,
+//     venue: "Pier 3 East ",
+//     location: "San Francisco, CA",
+//   },
+//   {
+//     date: 1634270400000,
+//     venue: "View Lounge",
+//     location: "San Francisco, CA",
+//   },
+//   {
+//     date: 1636171200000,
+//     venue: "Hyatt Agency",
+//     location: "San Francisco, CA",
+//   },
+//   {
+//     date: 1637902800000,
+//     venue: "Moscow Center",
+//     location: "San Francisco, CA",
+//   },
+//   {
+//     date: 1639544400000,
+//     venue: "Press Club",
+//     location: "San Francisco, CA",
+//   },
+// ];
+const link = "https://project-1-api.herokuapp.com/showdates";
+const apiKey = "?api_key=3e76ec52-db2a-4564-99d4-11b55158c9e7";
 
 const tableBody = document.querySelector(".table__body");
 
 showTable();
 
-function showTable () {
-    tableBody.innerHTML = ""
+function showTable() {
+  const getShows = axios.get(link + apiKey);
+  getShows.then((res) => {
+    const showsArrey = res.data;
+    tableBody.innerHTML = "";
     showsArrey.forEach((show) => {
-        const tableRow = document.createElement('tr');
-        tableRow.classList.add('table__row');
-        tableBody.appendChild(tableRow);
+      const tableRow = document.createElement("tr");
+      tableRow.classList.add("table__row");
+      tableBody.appendChild(tableRow);
 
-        const tableDate = document.createElement('td');
-        tableDate.classList.add("table__date");
-        tableDate.setAttribute('data-label', 'date');
-        const options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
-        tableDate.innerHTML = new Date(show.date).toLocaleDateString(
-            "en-US", options
-          );
-        tableRow.appendChild(tableDate);
+      const tableDate = document.createElement("td");
+      tableDate.classList.add("table__date");
+      tableDate.setAttribute("data-label", "date");
+      const options = {
+        weekday: "short",
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      };
+      tableDate.innerHTML = new Date(Number(show.date)).toLocaleDateString(
+        "en-US",
+        options
+      );
+      tableRow.appendChild(tableDate);
 
-        const tableVenue = document.createElement('td');
-        tableVenue.setAttribute('data-label', 'venue');
-        tableVenue.innerHTML = show.venue;
-        tableRow.appendChild(tableVenue);
+      const tableVenue = document.createElement("td");
+      tableVenue.setAttribute("data-label", "venue");
+      tableVenue.innerHTML = show.place;
+      tableRow.appendChild(tableVenue);
 
-        const tableCity = document.createElement('td');
-        tableCity.setAttribute('data-label', 'city');
-        tableCity.innerHTML = show.location;
-        tableRow.appendChild(tableCity);
+      const tableCity = document.createElement("td");
+      tableCity.setAttribute("data-label", "city");
+      tableCity.innerHTML = show.location;
+      tableRow.appendChild(tableCity);
 
-        const btnCell = document.createElement('td');
-        tableRow.appendChild(btnCell);
+      const btnCell = document.createElement("td");
+      tableRow.appendChild(btnCell);
 
-        const tableBtn = document.createElement('button');
-        tableBtn.classList.add('btn');
-        tableBtn.innerHTML = "buy tickets";
-        btnCell.appendChild(tableBtn);
+      const tableBtn = document.createElement("button");
+      tableBtn.classList.add("btn");
+      tableBtn.innerHTML = "buy tickets";
+      btnCell.appendChild(tableBtn);
     });
+  });
 }
 
-tableBody.addEventListener('click', (event)=>{
-    const selectedRow =event.target.parentElement;
-    selectedRow.classList.add('table__row--selected');
- 
-})
+tableBody.addEventListener("click", (event) => {
+  const selectedRow = event.target.parentElement;
+  selectedRow.classList.add("table__row--selected");
+});
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,105 +1,89 @@
-// const commentsArrey = [
-//   {
-//     name: "Connor Walton",
-//     dateCreated: 1613538000000,
-//     quote:
-//       "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-//   },
-//   {
-//     name: "Emilie Beach",
-//     dateCreated: 1610168400000,
-//     quote:
-//       "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-//   },
-//   {
-//     name: "Miles Acosta",
-//     dateCreated: 1608440400000,
-//     quote:
-//       "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-//   },
-// ];
-const link ="https://project-1-api.herokuapp.com/comments"
-const apiKey ="?api_key=3e76ec52-db2a-4564-99d4-11b55158c9e7"
 
+const link = "https://project-1-api.herokuapp.com/comments";
+const apiKey = "?api_key=3e76ec52-db2a-4564-99d4-11b55158c9e7";
 
-
-const form = document.querySelector(".form");
-const commentsList = document.querySelector(".comments__list");
-displayComments();
 
 function displayComments() {
-  const getComments = axios.get(link+apiKey)
-  getComments.then(res =>{
-    const commentsArrey = res.data;
-    console.log(commentsArrey);
+  const getComments = axios.get(link + apiKey);
+  getComments.then((res) => {
+    const commentsData = res.data;
+    const commentsArrey = commentsData.sort(
+      (a, b) => b.timestamp - a.timestamp
+    );
     commentsList.innerHTML = "";
     commentsArrey.forEach((comment) => {
       const commentLi = document.createElement("li");
       commentLi.classList.add("item");
       commentsList.appendChild(commentLi);
-  
+
       const itemImg = document.createElement("img");
       itemImg.classList.add("item__img");
       itemImg.setAttribute("src", " ");
       commentLi.appendChild(itemImg);
-  
+
       const itemSection = document.createElement("div");
       itemSection.classList.add("item__section");
       commentLi.appendChild(itemSection);
-  
+
       const itemHeader = document.createElement("div");
       itemHeader.classList.add("item__title");
       itemSection.appendChild(itemHeader);
-  
+
       const itemName = document.createElement("p");
       itemName.classList.add("item__name");
       itemName.innerHTML = comment.name;
       itemHeader.appendChild(itemName);
-  
+
       const itemDate = document.createElement("p");
       itemDate.classList.add("item__date");
       itemDate.innerText = new Date(comment.timestamp).toLocaleDateString(
         "en-US"
       );
       itemHeader.appendChild(itemDate);
-  
+
       const itemBody = document.createElement("div");
       itemBody.classList.add("item__body");
       itemSection.appendChild(itemBody);
-  
+
       const bodyCopy = document.createElement("p");
       bodyCopy.innerText = comment.comment;
       itemBody.appendChild(bodyCopy);
 
       const itemFooter = document.createElement("div");
-      itemFooter.classList.add("item__footer")
+      itemFooter.classList.add("item__footer");
       itemSection.appendChild(itemFooter);
 
       const likeImgLink = document.createElement("a");
-      likeImgLink.classList.add("item__actions","item__actions--like")
+      likeImgLink.classList.add("item__actions", "item__actions--like");
+      likeImgLink.setAttribute("data-id", comment.id);
+      likeImgLink.setAttribute("onclick", "liked(this)");
       itemFooter.appendChild(likeImgLink);
 
       const likeImg = document.createElement("img");
-      likeImg.classList.add('like-img');
+      likeImg.classList.add("like-img");
       likeImg.setAttribute("src", "./assets/Icons/SVG/icon-like.svg");
+      likeImg.setAttribute("data-id", comment.id);
       likeImgLink.appendChild(likeImg);
 
       const likeCounter = document.createElement("p");
-      likeImg.classList.add('like-counter');
+      likeImg.classList.add("like-counter");
       likeCounter.innerText = comment.likes;
+      likeCounter.setAttribute("data-id", comment.id);
       likeImgLink.appendChild(likeCounter);
 
       const deleteLink = document.createElement("a");
-      likeImgLink.classList.add("item__actions","item__actions--delete")
+      deleteLink.classList.add("item__actions", "item__actions--delete");
+      deleteLink.setAttribute("data-id", comment.id);
+      deleteLink.innerText = "Delete";
+      deleteLink.setAttribute("onclick", "deleteComment(this)");
       itemFooter.appendChild(deleteLink);
-
-      const deleteBtn = document.createElement("p");
-      deleteBtn.innerText = "Delete";
-      deleteLink.appendChild(deleteBtn);
     });
-  })
-
+  });
 }
+
+const form = document.querySelector(".form");
+const commentsList = document.querySelector(".comments__list");
+displayComments();
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -107,14 +91,27 @@ form.addEventListener("submit", (event) => {
   const submittedComment = event.target.formComment;
   const postedComment = {
     name: submittedName.value,
-    comment: submittedComment.value
+    comment: submittedComment.value,
   };
-
-  const addComment = axios.post(link+apiKey, postedComment)
-  addComment.then(res =>{
-    console.log(res);
-
+  const addComment = axios.post(link + apiKey, postedComment);
+  addComment.then((res) => {
+    event.target.reset();
+    displayComments();
   });
-  event.target.reset();
-  displayComments();
 });
+
+function liked(d){
+  const userId = d.getAttribute("data-id");
+  const addLike = axios.put(link+"/"+ userId+"/like"+apiKey)
+  addLike.then((res)=>{
+    displayComments()
+  })
+}
+
+function deleteComment(d){
+  const userId = d.getAttribute("data-id");
+  const addLike = axios.delete(link+"/"+ userId+apiKey)
+  addLike.then((res)=>{
+    displayComments()
+  })
+}
